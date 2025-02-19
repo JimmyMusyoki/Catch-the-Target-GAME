7fae5c95b200941bed480a3eb464453921288913
JimmyMusyoki <jimmy.mulundi@gmail.com>
2025-02-19 12:36:57 +0300

    npm install error

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MiniGame from "./components/MiniGame";
+
+function App() {
+  return (
+    <div className="App">
+      <MiniGame />
+    </div>
+  );
+}
+
+export default App;
